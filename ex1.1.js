058e0c670e67c29fc2cb96539c6845820df11cc1
7Hero <vpoopescu07@gmail.com>
2022-04-06 18:08:21 +0300

    logs

--- ex1.1.js
+++ ex1.1.js
@@ -13,7 +13,7 @@ const sortByName = (arr, type = 'asc') => {
   if(type == 'asc') nr = 1
   else if(type =='dsc') nr = -1
   else throw 'Type should be asc (Ascending) or dsc (Descending).'
-  
+
   return result.sort((a,b) => a.name > b.name ? nr : -nr )
 }
 
@@ -47,11 +47,11 @@ const sortedArray1 = sortByName(input,'asc')
 const sortedArray2 = sortByAge(input,'dsc')
 const sortedArray3 = sortByKeyName(input,'age','asc')
 
-console.log('Sorted by Name');
+console.log('Sorted by Name, asc');
 console.table(sortedArray1)
 
-console.log('Sorted by Age');
+console.log('Sorted by Age, dsc');
 console.table(sortedArray2)
 
-console.log('Sorted array');
+console.log('Sorted by Age, asc');
 console.table(sortedArray3);
