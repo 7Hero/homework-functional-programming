161854f3fd2ff159c57f459a6648f8b9bd8964e3
7Hero <49597813+7Hero@users.noreply.github.com>
2022-04-07 15:18:04 +0300

    Update ex1.3.js
    deleted some things

--- ex1.3.js
+++ ex1.3.js
@@ -20,6 +20,23 @@ const clonedObject = deepClone(input);
 clonedObject.a.b[1] = 3;
 console.log(input, clonedObject)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // --------------------------------------------------------------------------------------------------------------------------------------------------
 // linia 6  
 // if(typeof value == 'object'){ 
@@ -28,43 +45,3 @@ console.log(input, clonedObject)
 // }else{                                                                                                            clonedObj.g = value, face tot in else.
 //   clonedObj[key] = value               apoi cand termin for-ul o sa dau return la { f:value, g:value, h: {l:function(){}}} si tot asa. pana dau return
 // }                                      la un obiect clonat exact ca si obiectul original.
-
-const input1 = {
-  a: {
-    b: [1, 2, 3, 4],
-    c: {
-      d: {
-        f: 1,
-        g: 2,
-        h: {
-          l: function () {
-            console.log("Vasea");
-          }
-        }
-      }
-    }
-  }
-};
-// deepClone ver.2
-const deepClone = (obj) => {
-  let clonedObj = {};
-  // Cumva array tot ii object.
-  if (Array.isArray(obj)) {
-    return obj.map((value) =>
-      typeof value === "object" ? deepClone(value) : value
-    );
-  }
-  for (let [key, value] of Object.entries(obj)) {
-    // null tot cumva ii object.
-    typeof value === "object" && value != null
-      ? (clonedObj[key] = deepClone(value))
-      : (clonedObj[key] = value);
-  }
-  return clonedObj;
-};
-
-const clonedObject1 = deepClone(input);
-
-console.log(input1, clonedObject1);
-clonedObject.a = 4;
-console.log(input, clonedObject);
