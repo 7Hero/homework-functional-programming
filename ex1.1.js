c034bebeb377015553eef24de3fc01707c0e72db
7Hero <49597813+7Hero@users.noreply.github.com>
2022-04-07 15:46:56 +0300

    check if is array or not.

--- ex1.1.js
+++ ex1.1.js
@@ -8,8 +8,9 @@ let input = [
 
 //1.1.1 Sort by name 
 const sortByName = (arr, type = 'asc') => {
+  if(!Array.isArray(arr)) throw 'First argument is not an Array.'
   let result = [...arr], nr = 1;
-
+  
   if(type == 'asc') nr = 1
   else if(type =='dsc') nr = -1
   else throw 'Type should be asc (Ascending) or dsc (Descending).'
@@ -20,6 +21,7 @@ const sortByName = (arr, type = 'asc') => {
 
 //1.1.2 Sort by age
 const sortByAge = (arr,type) => {
+  if(!Array.isArray(arr)) throw 'First argument is not an Array.'
   let result = [...arr], nr = 1;
 
   if(type == 'asc') nr = 1
@@ -31,6 +33,7 @@ const sortByAge = (arr,type) => {
 
 //1.1.3 Sort by key name
 const sortByKeyName = (arr, keyName, type) => {
+  if(!Array.isArray(arr)) throw 'First argument is not an Array.'
   let result = [...arr], nr = 1;
   
   if(!Object.keys(arr[0]).includes(keyName)){
