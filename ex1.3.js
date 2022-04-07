f71c942c255f6765df0eab51680bc948d1aa43b4
7Hero <49597813+7Hero@users.noreply.github.com>
2022-04-07 15:04:59 +0300

    array check and null check to the deepClone function

--- ex1.3.js
+++ ex1.3.js
@@ -1,19 +1,24 @@
-const input = { a: { b: 1, c: { d: { f: 1, g: 2, h: { l: function(){console.log('Vasea')}} } } } };
+const input = { a: { b: [1,2,3,4], c: { d: { f: 1, g: 2, h: { l: function(){console.log('Vasea')}} } } } };
 
 const deepClone = (obj) => {
   let clonedObj = {}
+  //array tot ii object, fara check-ul asta b din input -> { 0:1, 1:2, 2:3, 3:4} noi avem nevoie de b: [1,2,3,4];
+  if (Array.isArray(obj)) {
+    return obj.map((value) =>
+      typeof value === "object" ? deepClone(value) : value
+    );
+  }
   for(let [key,value] of Object.entries(obj)){
-    typeof value == 'object' ? clonedObj[key] = deepClone(value) : clonedObj[key] = value;
+    //null tot ii object
+    typeof value == 'object' && value != null ? clonedObj[key] = deepClone(value) : clonedObj[key] = value;
   }
   return clonedObj;
 }
 
 const clonedObject = deepClone(input);
 
-clonedObject.a.b = 3;
-console.log(input, clonedObject) // input -> { a: { b: 1, c: { d: [Object] } } } clonedObject -> { a: { b: 3, c: { d: [Object] } } } 
-clonedObject.a = 4;
-console.log(input, clonedObject) 
+clonedObject.a.b[1] = 3;
+console.log(input, clonedObject)
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------
 // linia 6  
@@ -24,3 +29,42 @@ console.log(input, clonedObject)
 //   clonedObj[key] = value               apoi cand termin for-ul o sa dau return la { f:value, g:value, h: {l:function(){}}} si tot asa. pana dau return
 // }                                      la un obiect clonat exact ca si obiectul original.
 
+const input1 = {
+  a: {
+    b: [1, 2, 3, 4],
+    c: {
+      d: {
+        f: 1,
+        g: 2,
+        h: {
+          l: function () {
+            console.log("Vasea");
+          }
+        }
+      }
+    }
+  }
+};
+// deepClone ver.2
+const deepClone = (obj) => {
+  let clonedObj = {};
+  // Cumva array tot ii object.
+  if (Array.isArray(obj)) {
+    return obj.map((value) =>
+      typeof value === "object" ? deepClone(value) : value
+    );
+  }
+  for (let [key, value] of Object.entries(obj)) {
+    // null tot cumva ii object.
+    typeof value === "object" && value != null
+      ? (clonedObj[key] = deepClone(value))
+      : (clonedObj[key] = value);
+  }
+  return clonedObj;
+};
+
+const clonedObject1 = deepClone(input);
+
+console.log(input1, clonedObject1);
+clonedObject.a = 4;
+console.log(input, clonedObject);
